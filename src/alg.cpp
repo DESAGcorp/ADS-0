dfc7787005472ea220c122cc5c6a9b270e7e6340
DESAGcorp <79166846+DESAGcorp@users.noreply.github.com>
2021-03-20 11:58:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,15 @@
 // Copyright 2021 NNTU-CS
 
-int gcd(int a, int b) 
-{
+int gcd(int a, int b) {
     int div;
     int c = a - b;
-
     if (a == b)
         return a;
-
-    if (c < 0)
-    {
+    if (c < 0) {
         c = -c;
         div = gcd(a, c);
     }
     else
         div = gcd(b, c);
-    
     return div;
 }
